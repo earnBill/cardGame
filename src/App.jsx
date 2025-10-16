74aa60aa5fbb42a52a878e7548a5baa19613eeae
billTas <billtas@gmail.com>
2025-10-16 13:06:59 +0300

    create shuffle function

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,22 @@ function App() {
       }
     }
     getPokemon();
-  },[]) 
+  },[serverUrl]) 
+
+  function randomOrder(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      //destructuring 
+      // [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      let temp = shuffled[i];
+      shuffled[i] = shuffled[j];
+      shuffled[j] = temp;
+      console.log(shuffled[i]);
+    }
+    console.log(shuffled);
+    setPokemons(shuffled);
+  }
 
   return (
     <>
@@ -43,6 +58,7 @@ function App() {
         )
         })
       }
+      <button onClick={() => randomOrder(pokemons)}>Shuffle</button>
       
     </>
   )
